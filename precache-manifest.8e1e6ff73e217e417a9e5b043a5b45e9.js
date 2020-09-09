de79994f450c0ad4d5b924a7744a174c7f52d5e1
ihor <malishkoio@gmail.com>
2020-09-09 22:10:04 +0300

    Updates

---
+++ precache-manifest.8e1e6ff73e217e417a9e5b043a5b45e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80eeda921f8a4e2b75b4e263b6679c9e",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6f58b6431e88b2f28779",
+    "url": "/react-hw-01-components/static/css/main.743ecdb6.chunk.css"
+  },
+  {
+    "revision": "a15272da4b08b214d191",
+    "url": "/react-hw-01-components/static/js/2.899f9c0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01-components/static/js/2.899f9c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f58b6431e88b2f28779",
+    "url": "/react-hw-01-components/static/js/main.6a4a87f7.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  }
+]);
